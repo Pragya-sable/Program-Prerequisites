fe3c952371bed453a8ffe8f46cf259bd564b358b
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-01-08 00:07:43 +0530

    Question2

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,6 +1,6 @@
 import java.util.*;
 class AddTwoNumbers{
-public static void main{String args[]){
+public static void main(String args[]){
 Scanner kb=new Scanner(System.in);
 int a=kb.nextInt();
 int b=kb.nextInt();
