a2ad2c37faba06df400859dc182fa0dd5c2a9868
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-01-08 00:25:24 +0530

    Question6

---
+++ CalculateSimpleInterest.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class CalculateSimpleInterest{
+public static void main(String args[]){
+Scanner kb=new Scanner(System.in);
+int p=kb.nextInt();
+int r=kb.nextInt();
+int t=kb.nextInt();
+double SI=(p*t*r)/100;
+
+
+System.out.println("SI is "+ SI);
+}
+}
