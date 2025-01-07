f6b12f2349f3fbbe208a6317c2c62297a2cdc15c
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-01-07 23:29:29 +0530

    Question2

---
+++ AddTwoNumbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class AddTwoNumbers{
+public static void main{String args[]){
+Scanner kb=new Scanner(System.in);
+int a=kb.nextInt();
+int b=kb.nextInt();
+int sum=0;
+sum=a+b;
+System.out.println("Sum is "+ sum);
+}
+}
