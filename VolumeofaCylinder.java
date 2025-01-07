98a223e9d2e9b4c995d320594bcaaf66b3f2ae28
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-01-08 00:38:31 +0530

    Question5

--- VolumeofaCylinder.java
+++ VolumeofaCylinder.java
@@ -3,8 +3,8 @@ import java.util.*;
 class VolumeofaCylinder{
 public static void main(String args[]){
 Scanner kb=new Scanner(System.in);
-int radius=kb.nextInt();
-int height=kb.nextInt();
+double radius=kb.nextDouble();
+double height=kb.nextDouble();
 
 double vol=3.14*radius*radius*height;
 System.out.println("Volume of Cylinder is "+ vol);
