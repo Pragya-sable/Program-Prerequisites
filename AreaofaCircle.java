c0d3557310995e699468b4471503304c6b4067b2
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-01-08 00:36:18 +0530

    Question4

--- AreaofaCircle.java
+++ AreaofaCircle.java
@@ -3,7 +3,7 @@ import java.util.*;
 class AreaofaCircle{
 public static void main(String args[]){
 Scanner kb=new Scanner(System.in);
-int radius=kb.nextInt();
+double radius=kb.nextDouble();
 
 double area=3.14*radius*radius;
 System.out.println("Area of Circle is "+ area);
