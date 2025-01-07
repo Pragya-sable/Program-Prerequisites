26fd04cba5d0969f395c02f3d3c10c76552b5224
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-01-08 00:33:33 +0530

    Question7

---
+++ PerimeterofaRectangle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class PerimeterofaRectangle{
+public static void main(String args[]){
+Scanner kb=new Scanner(System.in);
+double l=kb.nextDouble();
+double b=kb.nextDouble();
+
+double p=2*(l+b);
+
+
+System.out.println("Perimeter is "+ p);
+}
+}
