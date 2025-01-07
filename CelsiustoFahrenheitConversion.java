b40a39c0494533aaa4fa4626541c1d1491b5baa2
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-01-08 00:09:40 +0530

    Question3

--- CelsiustoFahrenheitConversion.java
+++ CelsiustoFahrenheitConversion.java
@@ -1,7 +1,7 @@
 
 import java.util.*;
 class CelsiustoFahrenheitConversion{
-public static void main{String args[]){
+public static void main(String args[]){
 Scanner kb=new Scanner(System.in);
 double cel=kb.nextDouble();
 double ferh=(cel*9/5)+32;
