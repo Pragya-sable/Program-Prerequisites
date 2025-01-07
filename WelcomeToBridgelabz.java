992b51ffa46eb653b4e5bf14ffc723a5941c7029
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-01-08 00:03:57 +0530

    Question1

--- WelcomeToBridgelabz.java
+++ WelcomeToBridgelabz.java
@@ -1,6 +1,6 @@
 import java.util.*;
 class WelcomeToBridgelabz{
-public static void main{String args[]){
+public static void main(String args[]){
 System.out.println("Welcome to Bridgelabz!");
 }
 }
