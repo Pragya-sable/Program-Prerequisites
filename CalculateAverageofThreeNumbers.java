5426987f05328168d9f1d0b8b340b1f1e382030e
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-01-08 00:54:38 +0530

    Question9

---
+++ CalculateAverageofThreeNumbers.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+class CalculateAverageofThreeNumbers{
+public static void main(String args[]){
+Scanner kb=new Scanner(System.in);
+double p=kb.nextDouble();
+double r=kb.nextDouble();
+double t=kb.nextDouble();
+double avg=(p+t+r)/3;
+
+
+System.out.println("Average is "+ avg);
+}
+}
